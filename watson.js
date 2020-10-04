83562c63255b2844e24501e435774f836f506d40
rahmanapyrr <rahmana.muhammad@bison.howard.edu>
2020-10-04 08:48:02 -0400

    added server for IBM Watson

--- watson.js
+++ watson.js
@@ -12,15 +12,17 @@ const authenticator = new IamAuthenticator({apikey: process.env.WATSON_ASSISTANT
     serviceUrl: process.env.WATSON_ASSISTANT_URL,
   });
 
-  router.get{"/session", async (res,res)=>{
-      try{
-          const session = await assistant.createSession ({
-              assistantId: process.env.WATSON_ASSISTANT_ID,
-          });
-          res.json(session["result"]);
-      }
-      catch (error){
-          res.send("There was an error in your request");
-          console.log(err);
-      }
-    }};
+//   router.get{"/session", async (res,res)=>{
+//       try{
+//           const session = await assistant.createSession ({
+//               assistantId: process.env.WATSON_ASSISTANT_ID,
+//           });
+//           res.json(session["result"]);
+//       }
+//       catch (error){
+//           res.send("There was an error in your request");
+//           console.log(err);
+//       }
+//     }};
+
+//     module.exports = router;
